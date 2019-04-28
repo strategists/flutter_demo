733ee558851c07fdf615bef3f5a420aed1796e14
Puming <pmchongqing@163.com>
2019-04-28 18:28:06 +0800

    modify media_page

--- lib/media/media_page.dart
+++ lib/media/media_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/widget/gather_page.dart';
 import 'package:flutter_demo/rtc/trtc_page.dart';
+import 'package:flutter_demo/style/my_color.dart';
+import 'package:android_intent/android_intent.dart';
+import 'dart:async';
+import 'dart:io';
 
 class MediaPage extends GatherPage {
-  MediaPage() : super(content:MediaListBody(),title:"图像采集",targetPage:TrtcPage());
+  MediaPage()
+      : super(content: MediaListBody(), title: "图像采集", targetPage: TrtcPage());
 }
 
 class MediaListBody extends StatefulWidget {
@@ -15,6 +20,7 @@ class MediaListBody extends StatefulWidget {
 
 class _MediaListBodyState extends State<MediaListBody> {
   var _list = <String>[];
+  var _outlineButtonText = '立即拍照';
 
   ScrollController _scrollController;
 
@@ -32,7 +38,6 @@ class _MediaListBodyState extends State<MediaListBody> {
     // TODO: implement build
     return _buildList(context);
   }
-
 
   Widget _buildList(BuildContext context) {
 //    assert(_list = null);
@@ -72,44 +77,96 @@ class _MediaListBodyState extends State<MediaListBody> {
             children: <Widget>[
               Row(
                 mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   Align(
                     alignment: Alignment.centerLeft,
-                    child: Text(
-                      "重庆中链融",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                      ),
-                    ),
+                    child: Icon(Icons.picture_in_picture),
                   ),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 5.0)),
                   Align(
-                    alignment: Alignment.centerRight,
+                    alignment: Alignment.centerLeft,
                     child: Text(
-                      "下户中",
-                      style: TextStyle(color: Colors.amber, fontSize: 12.0),
+                      "小区大门照",
+                      style: TextStyle(color: Colors.amber, fontSize: 14.0),
                     ),
                   ),
                 ],
               ),
               Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text(
-                    "客户经理【张三】，邀您进行远程下户",
-                    style: TextStyle(fontSize: 12.0),
+                  Image.asset(
+                    'images/perloader_icon.png',
+                    height: 96,
+                    width: 96,
                   ),
-                  Icon(Icons.keyboard_arrow_right)
+//                  VerticalDivider(color: const Color(0xFFD9DFE7),),
+                  SizedBox(
+                    width: 0.0,
+                    height: 96.0,
+                    child: Center(
+                      child: Container(
+                        width: 0.0,
+                        margin: EdgeInsetsDirectional.only(start: 0),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            left: Divider.createBorderSide(
+                              context,
+                              color: const Color(0xFFD9DFE7),
+                              width: 1.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Image.asset(
+                    'images/perloader_icon.png',
+                    height: 96,
+                    width: 96,
+                  ),
+                  OutlineButton(
+                    onPressed: () {
+                      if (Platform.isAndroid) {
+                        AndroidIntent intent = AndroidIntent(
+                          action: 'action_view',
+                          data: 'https://play.google.com/store/apps/details?'
+                              'id=com.google.android.apps.myapp',
+                          arguments: {'authAccount': ""},
+                        );
+                        intent.launch();
+                      }
+                    },
+                    child: Text(
+                      _outlineButtonText,
+                      style: TextStyle(
+                        color: MyColors.primary(),
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    borderSide: BorderSide(
+                      color: MyColors.primary(),
+                      width: 2.0,
+                      style: BorderStyle.solid,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0),
+                      ),
+                    ),
+                  ),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  Icon(Icons.home),
                   Text(
-                    "光电园",
-                    style: TextStyle(fontSize: 12),
+                    "注意：图片需可见小区名、街道号等，至少1张",
+                    style:
+                        TextStyle(fontSize: 12, color: const Color(0xFF878787)),
                   )
                 ],
               ),
---
+++ lib/style/my_color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static Color primary() => const Color(0xFF5588FD);
+}
