ed8447e2f6eaf3a6f7a94bde22b8af7b2de8fb5c
Puming <pmchongqing@163.com>
2019-06-14 14:49:52 +0800

    modify gallery page

--- lib/image_picker/gallery.dart
+++ lib/image_picker/gallery.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:multi_image_picker/multi_image_picker.dart';
 
 class Gallery extends StatefulWidget {
   @override
@@ -9,54 +8,13 @@ class Gallery extends StatefulWidget {
 }
 
 class _GalleryState extends State<Gallery> {
-  List<Asset> images = List<Asset>();
-  String _error;
+
 
   @override
   void initState() {
     super.initState();
   }
 
-  Widget buildGridView() {
-    return GridView.count(
-      crossAxisCount: 3,
-      children: List.generate(0, (index) {
-        Asset asset = images[index];
-        return AssetThumb(
-          asset: asset,
-          width: 300,
-          height: 300,
-        );
-      }),
-    );
-  }
-
-  Future<void> loadAssets() async {
-    setState(() {
-      images = List<Asset>();
-    });
-
-    List<Asset> resultList;
-    String error;
-
-    try {
-      resultList = await MultiImagePicker.pickImages(
-        maxImages: 300,
-      );
-    } catch (e) {
-      error = e.message;
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      images = resultList;
-      if (error == null) _error = 'No Error Dectected';
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,18 +22,7 @@ class _GalleryState extends State<Gallery> {
       appBar: new AppBar(
         title: const Text('Plugin example app'),
       ),
-      body: Column(
-        children: <Widget>[
-          Center(child: Text('Error: $_error')),
-          RaisedButton(
-            child: Text("Pick images"),
-            onPressed: loadAssets,
-          ),
-          Expanded(
-            child: buildGridView(),
-          )
-        ],
-      ),
+      body:Container(),
     );
   }
 }
